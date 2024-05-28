92cd398215adb64ee1f7bb06262d77545b71550b
ardasakarya <sakaryaarda@gmail.com>
2024-05-28 15:53:16 +0300

    mesajlaşma için bazı UI lar eklendi

--- app/src/main/java/com/example/dateapp/messageActivity.kt
+++ app/src/main/java/com/example/dateapp/messageActivity.kt
@@ -1,11 +1,27 @@
 package com.example.dateapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class messageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message)
     }
+
+
+    fun send (view: View)
+    {
+        Toast.makeText(this, "mesaj gönderildi", Toast.LENGTH_SHORT).show()
+    }
+
+    fun messageBack(view: View)
+    {
+        intent = Intent(applicationContext,rightSwipeActivity::class.java)
+        startActivity(intent)
+        finish()
+            }
 }
